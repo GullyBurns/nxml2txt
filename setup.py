a0d6eede152571545ca35ec0b29c5270c05ed9dd
Gully Burns <gully.burns@chanzuckerberg.com>
2021-04-20 22:01:19 -0700

    hacks to get the data files in the right place"

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
+import os
+import sys
 
+data_dir = os.path.join(sys.prefix, "local/lib/python2.7/dist-package/my_module")
+data_files   = [ ("my_module",  [os.path.join(data_dir, "data1"),
+                                 os.path.join(data_dir, "data2")])]
 setup(
     name='nxml_2_txt',
     version='1.0.1',
     packages=['src'],
-    data_files=[('../data', ['data/entities.dat'])],
+    data_dir = os.path.join(sys.prefix, "local/lib/python3.7/site-packages/data/"),
+    data_files=[("data", [os.path.join(data_dir, "entities.dat'")])],
     url='https://github.com/GullyBurns/nxml2txt',
     license='MIT ',
     author='orig. Sampo Pyysalo; updated: Gully Burns',
